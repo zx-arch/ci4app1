3bdd7ef187f616616f55c0346f00b0dd3064cc5d
zx-arch <69253132+zx-arch@users.noreply.github.com>
2021-07-17 15:43:15 +0700

    Create bad_url.php

---
+++ app/Views/bad_url.php
@@ -0,0 +1,19 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.css">
+
+
+<script>
+    $(function() {
+
+        <?php if (session()->has("warning")) : ?>
+            Swal.fire({
+                icon: 'warning',
+                title: 'Perhatian!',
+                text: 'URL "<?php echo base_url('/naruto'); ?>/<?= session("warning") ?>" Tidak Dapat Diakses'
+            }).then((result) => {
+                window.location = '<?php echo base_url('/naruto'); ?>';
+            })
+        <?php endif; ?>
+    })
+</script>
