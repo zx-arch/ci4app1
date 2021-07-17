ea6c8891845dfbc0565b51d2a72a802fbd38de33
zx-arch <69253132+zx-arch@users.noreply.github.com>
2021-07-17 15:53:16 +0700

    Create getallmanga.php

---
+++ app/Views/layout/getallmanga.php
@@ -0,0 +1,69 @@
+<?php if ($data['slug'] === 'naruto') : ?>
+    <span style="font-weight: bold;"><?= $genre[0]['genre']["naruto"]; ?></span>
+<?php elseif ($data['slug'] === 'one-piece') : ?>
+    <span style="font-weight: bold;"><?= $genre[1]['genre']["one-piece"]; ?></span>
+<?php elseif ($data['slug'] === 'ao-no-exorcist') : ?>
+    <span style="font-weight: bold;"><?= $genre[2]['genre']["ao-no-exorcist"]; ?></span>
+<?php elseif ($data['slug'] === 'rising-x-rydeen') : ?>
+    <span style="font-weight: bold;"><?= $genre[3]['genre']["rising-x-rydeen"]; ?></span>
+<?php elseif ($data['slug'] === 'kemono-giga') : ?>
+    <span style="font-weight: bold;"><?= $genre[4]['genre']["kemono-giga"]; ?></span>
+<?php elseif ($data['slug'] === 'shuumatsu-no-valkyrie') : ?>
+    <span style="font-weight: bold;"><?= $genre[5]['genre']["shuumatsu-no-valkyrie"]; ?></span>
+<?php elseif ($data['slug'] === 'monster-x-monster') : ?>
+    <span style="font-weight: bold;"><?= $genre[6]['genre']["monster-x-monster"]; ?></span>
+<?php elseif ($data['slug'] === 'boruto') : ?>
+    <span style="font-weight: bold;"><?= $genre[7]['genre']["boruto"]; ?></span>
+<?php elseif ($data['slug'] === 'one-punch-man') : ?>
+    <span style="font-weight: bold;"><?= $genre[8]['genre']["one-punch-man"]; ?></span>
+<?php elseif ($data['slug'] === 'tokyo-ghoul') : ?>
+    <span style="font-weight: bold;"><?= $genre[9]['genre']["tokyo-ghoul"]; ?></span>
+<?php elseif ($data['slug'] === 'world-trigger') : ?>
+    <span style="font-weight: bold;"><?= $genre[10]['genre']["world-trigger"]; ?></span>
+<?php elseif ($data['slug'] === 'black-clover') : ?>
+    <span style="font-weight: bold;"><?= $genre[11]['genre']["black-clover"]; ?></span>
+<?php elseif ($data['slug'] === 'kokoro-irozuku-koi-ga-shitai') : ?>
+    <span style="font-weight: bold;"><?= $genre[12]['genre']["kokoro-irozuku-koi-ga-shitai"]; ?></span>
+<?php elseif ($data['slug'] === 'bleach') : ?>
+    <span style="font-weight: bold;"><?= $genre[13]['genre']["bleach"]; ?></span>
+<?php elseif ($data['slug'] === 'the-revenge-of-the-soul-eater') : ?>
+    <span style="font-weight: bold;"><?= $genre[14]['genre']["the-revenge-of-the-soul-eater"]; ?></span>
+<?php elseif ($data['slug'] === 'juujika-no-rokunin') : ?>
+    <span style="font-weight: bold;"><?= $genre[15]['genre']["juujika-no-rokunin"]; ?></span>
+<?php elseif ($data['slug'] === 'radiant') : ?>
+    <span style="font-weight: bold;"><?= $genre[16]['genre']["radiant"]; ?></span>
+<?php elseif ($data['slug'] === 'colorless') : ?>
+    <span style="font-weight: bold;"><?= $genre[17]['genre']["colorless"]; ?></span>
+<?php elseif ($data['slug'] === 'nidome-no-jinsei-wo-isekai-de') : ?>
+    <span style="font-weight: bold;"><?= $genre[26]['genre']["nidome-no-jinsei-wo-isekai-de"]; ?></span>
+<?php elseif ($data['slug'] === 'blood-lad') : ?>
+    <span style="font-weight: bold;"><?= $genre[33]['genre']["blood-lad"]; ?></span>
+<?php elseif ($data['slug'] === 'akame-ga-kill') : ?>
+    <span style="font-weight: bold;"><?= $genre[41]['genre']["akame-ga-kill"]; ?></span>
+<?php elseif ($data['slug'] === 'edens-zero') : ?>
+    <span style="font-weight: bold;"><?= $genre[48]['genre']["edens-zero"]; ?></span>
+<?php elseif ($data['slug'] === 'zipman') : ?>
+    <span style="font-weight: bold;"><?= $genre[52]['genre']["zipman"]; ?></span>
+<?php elseif ($data['slug'] === 'pseudo-harem') : ?>
+    <span style="font-weight: bold;"><?= $genre[56]['genre']["pseudo-harem"]; ?></span>
+<?php elseif ($data['slug'] === '7th-garden') : ?>
+    <span style="font-weight: bold;"><?= $genre[58]['genre']["7th-garden"]; ?></span>
+<?php elseif ($data['slug'] === 'vagabond') : ?>
+    <span style="font-weight: bold;"><?= $genre[66]['genre']["vagabond"]; ?></span>
+<?php elseif ($data['slug'] === 'vigilante-boku-no-hero-academia-illegal') : ?>
+    <span style="font-weight: bold;"><?= $genre[69]['genre']["vigilante-boku-no-hero-academia-illegal"]; ?></span>
+<?php elseif ($data['slug'] === 'world-of-immortals') : ?>
+    <span style="font-weight: bold;"><?= $genre[72]['genre']["world-of-immortals"]; ?></span>
+<?php elseif ($data['slug'] === 'uq-holder') : ?>
+    <span style="font-weight: bold;"><?= $genre[78]['genre']["uq-holder"]; ?></span>
+<?php elseif ($data['slug'] === '666-satan') : ?>
+    <span style="font-weight: bold;"><?= $genre[86]['genre']["666-satan"]; ?></span>
+<?php elseif ($data['slug'] === 'hahasama-no-ie-ogamiya-gounai-kokoro-hitomi-no-kaiitan') : ?>
+    <span style="font-weight: bold;"><?= $genre[89]['genre']["hahasama-no-ie-ogamiya-gounai-kokoro-hitomi-no-kaiitan"]; ?></span>
+<?php elseif ($data['slug'] === 'fairy-tail-100-years-quest') : ?>
+    <span style="font-weight: bold;"><?= $genre[96]['genre']["fairy-tail-100-years-quest"]; ?></span>
+<?php elseif ($data['slug'] === 'jagaaaaaan') : ?>
+    <span style="font-weight: bold;"><?= $genre[102]['genre']["jagaaaaaan"]; ?></span>
+<?php elseif ($data['slug'] === 'dragon-ball-super') : ?>
+    <span style="font-weight: bold;"><?= $genre[109]['genre']["dragon-ball-super"]; ?></span>
+<?php endif; ?>
